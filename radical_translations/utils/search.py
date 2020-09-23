83f2c60f428fe29dd13af31d2f76c33b925c76c2
Miguel Vieira <jmvieira@pm.me>
2020-09-23 14:46:05 +0100

    📦 New: Add custom pagination class for search. It adds page size and current page to the search data.

---
+++ radical_translations/utils/search.py
@@ -0,0 +1,16 @@
+from django_elasticsearch_dsl_drf.pagination import (
+    PageNumberPagination as BasePageNumberPagination,
+)
+
+
+# https://django-elasticsearch-dsl-drf.readthedocs.io/en/latest/advanced_usage_examples.html?highlight=size#customisations
+class PageNumberPagination(BasePageNumberPagination):
+    """Custom page number pagination."""
+
+    def get_paginated_response_context(self, data):
+        __data = super().get_paginated_response_context(data)
+        __data.append(("current_page", int(self.request.query_params.get("page", 1))))
+        __data.append(("page_size", self.get_page_size(self.request)))
+        __data.append(("ordering", self.request.query_params.get("ordering", "")))
+
+        return sorted(__data)
