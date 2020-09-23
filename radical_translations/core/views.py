1dc07b0cb522378fbb738b0610fdd12ab6e27739
Miguel Vieira <jmvieira@pm.me>
2020-09-23 14:51:20 +0100

    👌 Improve: Add ordering fields and pagination class.

--- radical_translations/core/views.py
+++ radical_translations/core/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.views.generic.detail import DetailView
-from django_elasticsearch_dsl_drf.filter_backends import SearchFilterBackend
+from django_elasticsearch_dsl_drf.filter_backends import (
+    DefaultOrderingFilterBackend,
+    OrderingFilterBackend,
+    SearchFilterBackend,
+)
 from django_elasticsearch_dsl_drf.viewsets import DocumentViewSet
 
 from radical_translations.core.documents import ResourceDocument
 from radical_translations.core.models import Resource
 from radical_translations.core.serializers import ResourceDocumentSerializer
+from radical_translations.utils.search import PageNumberPagination
 
 
 class ResourceDetailView(DetailView):
@@ -20,8 +25,17 @@ class ResourceViewSet(DocumentViewSet):
     document = ResourceDocument
     serializer_class = ResourceDocumentSerializer
 
-    filter_backends = [SearchFilterBackend]
+    filter_backends = [
+        DefaultOrderingFilterBackend,
+        OrderingFilterBackend,
+        SearchFilterBackend,
+    ]
+
     lookup_field = "id"
-    ordering = ["title.main_title", "date_earliest", "date_latest"]
-    page_size = 50
+
+    ordering_fields = {"title": "title.main_title.raw", "date": "date_earliest"}
+    ordering = ["title", "date"]
+
+    pagination_class = PageNumberPagination
+
     search_fields = ["title.main_title", "languages.language.label"]
