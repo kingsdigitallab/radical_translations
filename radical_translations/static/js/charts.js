809e63ca006e6a0bc4fd384591dfd58feb24a550
Miguel Vieira <jmvieira@pm.me>
2021-01-27 14:57:54 +0000

    👌 Improve: Add details to the tooltip.

--- radical_translations/static/js/charts.js
+++ radical_translations/static/js/charts.js
@@ -47,10 +47,19 @@ Vue.component('bubble-chart', {
       },
       tooltips: {
         callbacks: {
+          title: function (tooltipItem, data) {
+            const meta =
+              data.datasets[tooltipItem[0].datasetIndex].data[
+                tooltipItem[0].index
+              ].meta
+            return `${meta.date} ${meta.title}`
+          },
           label: function (tooltipItem, data) {
-            const item = data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+            const meta =
+              data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                .meta
 
-            return `${item.x}, ${item.title}: ${item.resources}`
+            return `${meta.place}; ${meta.resources} resources`
           }
         }
       },
