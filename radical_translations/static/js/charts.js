a4db50b569587e77cb58fa00e0bb42f749279290
Miguel Vieira <jmvieira@pm.me>
2021-02-02 17:48:13 +0000

    🐛 Fix: Make chart options reactive.

--- radical_translations/static/js/charts.js
+++ radical_translations/static/js/charts.js
@@ -2,26 +2,30 @@ Vue.component('bar-chart', {
   extends: VueChartJs.Bar,
   props: ['clickHandler'],
   mixins: [VueChartJs.mixins.reactiveProp],
-  mounted() {
-    const self = this
-    this.renderChart(this.chartData, {
-      legend: { display: false },
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true
+  data: function () {
+    return {
+      options: {
+        legend: { display: false },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true
+              }
             }
+          ]
+        },
+        onClick: function (evt, item) {
+          if (item.length > 0) {
+            const year = item[0]['_model'].label
+            this.clickHandler(year, year)
           }
-        ]
-      },
-      onClick: function (evt, item) {
-        if (item.length > 0) {
-          const year = item[0]['_model'].label
-          self.clickHandler(year, year)
         }
       }
-    })
+    }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
   }
 })
 
@@ -29,60 +33,53 @@ Vue.component('bubble-chart', {
   extends: VueChartJs.Bubble,
   props: ['clickHandler'],
   mixins: [VueChartJs.mixins.reactiveProp],
-  mounted() {
-    const self = this
-    this.renderChart(this.chartData, {
-      legend: { display: true },
-      scales: {
-        yAxes: [
-          {
-            ticks: {
-              beginAtZero: true,
-              callback: function (value, index, values) {
-                return self.chartData.labels[value]
+  data: function () {
+    return {
+      options: {
+        legend: { display: true },
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: true,
+                callback: function (value, index, values) {
+                  return this.chart.data.labels[value]
+                }
               }
             }
+          ]
+        },
+        tooltips: {
+          callbacks: {
+            title: function (tooltipItem, data) {
+              const item =
+                data.datasets[tooltipItem[0].datasetIndex].data[
+                  tooltipItem[0].index
+                ]
+
+              return `${item.meta.place}, ${item.x}`
+            },
+            label: function (tooltipItem, data) {
+              const meta =
+                data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
+                  .meta
+
+              return `${meta.date} ${meta.title}; ${meta.resources} resources`
+            }
           }
-        ]
-      },
-      tooltips: {
-        callbacks: {
-          title: function (tooltipItem, data) {
-            const item =
-              data.datasets[tooltipItem[0].datasetIndex].data[
-                tooltipItem[0].index
-              ]
-
-            return `${item.meta.place}, ${item.x}`
-          },
-          label: function (tooltipItem, data) {
-            const meta =
-              data.datasets[tooltipItem.datasetIndex].data[tooltipItem.index]
-                .meta
-
-            return `${meta.date} ${meta.title}; ${meta.resources} resources`
+        },
+        onClick: function (evt, item) {
+          if (item.length > 0) {
+            const meta = this.chart.data.datasets[item[0]._datasetIndex].data[
+              item[0]._index
+            ].meta
+            location.href = meta.id
           }
         }
-      },
-      onClick: function (evt, item) {
-        if (item.length > 0) {
-          const year = item[0]['_model'].label
-          self.clickHandler(year, year)
-        }
       }
-    })
-  },
-  methods: {
-    generateData: function () {
-      var data = []
-      for (var i = 0; i < 7; i++) {
-        data.push({
-          x: i + i,
-          y: i,
-          r: i
-        })
-      }
-      return data
     }
+  },
+  mounted() {
+    this.renderChart(this.chartData, this.options)
   }
 })
