bd34033fe5d99be41f22aadae9ec123ed11fb597
Miguel Vieira <jmvieira@pm.me>
2021-05-07 17:33:49 +0100

    📦 New: Add events timeline grid view.

--- radical_translations/events/urls.py
+++ radical_translations/events/urls.py
@@ -4,8 +4,9 @@ from rest_framework.routers import DefaultRouter
 from radical_translations.events.views import (
     EventDetailView,
     EventViewSet,
-    event_list,
     TimelineMockupPageView,
+    event_grid,
+    event_list,
 )
 
 router = DefaultRouter()
@@ -14,5 +15,6 @@ router.register("api", basename="event-api", viewset=EventViewSet)
 urlpatterns = [
     path("<int:pk>/", EventDetailView.as_view(), name="event-detail"),
     path("", event_list, name="event-list"),
+    path("grid/", event_grid, name="event-grid"),
     path("timeline-mockup/", TimelineMockupPageView.as_view(), name="timeline-mockup"),
 ] + router.urls
