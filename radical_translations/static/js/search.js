af02094538453fd5a902d614634c57901817f8c2
Miguel Vieira <jmvieira@pm.me>
2022-10-06 11:41:32 +0100

    🐛 Fix: Force https url for pagination when on production servers.

--- radical_translations/static/js/search.js
+++ radical_translations/static/js/search.js
@@ -359,7 +359,7 @@ new Vue({
         );
 
         url = json.next;
-        url = url.indexOf("radicalt") ? url.replace("http:", "https:") : url
+        url = url.indexOf("radicalt") > 0 ? url.replace("http:", "https:") : url
       } while (url && all);
 
       return json;
